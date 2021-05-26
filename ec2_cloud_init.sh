1111ab9c424880cdd4e4771bcd97bc82b7fa3b81
Darren Weber <darren.weber@jupiterintel.com>
2021-05-25 22:21:31 -0700

    Fix jq installation

--- ec2_cloud_init.sh
+++ ec2_cloud_init.sh
@@ -53,8 +53,8 @@ sudo ./aws/install --update
 # Install jq (JSON Query)
 #
 curl -o jq-linux64 -sSL https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
-mv jq-linux64 /usr/local/bin/jq
-chmod a+x /usr/local/bin/jq
+sudo mv jq-linux64 /usr/local/bin/jq
+sudo chmod a+x /usr/local/bin/jq
 jq --version
 
 
